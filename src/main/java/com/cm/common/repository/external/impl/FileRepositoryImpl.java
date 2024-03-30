9460638b43456ed39611b76f79177d40d0c4f505
andriyMyk <162585299+andriyMyk@users.noreply.github.com>
2024-03-30 18:46:36 +0200

    Profiled pom config

--- src/main/java/com/cm/common/repository/external/impl/FileRepositoryImpl.java
+++ src/main/java/com/cm/common/repository/external/impl/FileRepositoryImpl.java
@@ -1,76 +1,76 @@
-package com.cm.common.repository.external.impl;
-
-import com.cm.common.exception.SystemException;
-import com.cm.common.model.enumeration.MediaUploadStatus;
-import com.cm.common.repository.external.FileRepository;
-import com.cm.common.security.AppUserDetails;
-import com.cm.common.util.AuthorizationUtil;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-@Slf4j
-@Repository
-@RequiredArgsConstructor
-public class FileRepositoryImpl implements FileRepository {
-
-    private final Bucket homeworkBucket;
-    private final Storage storage;
-
-    @Value("${google.cloud.credentials-json-path}")
-    private String credentialsJsonPath;
-
-    @Override
-    public MediaUploadStatus uploadFile(final MultipartFile file, final String key) {
-        final AppUserDetails currentUser = (AppUserDetails) AuthorizationUtil.getCurrentUser();
-        try {
-            homeworkBucket.create(key, file.getInputStream());
-            log.info("Homework uploaded for user: {}", currentUser.getUsername());
-            return MediaUploadStatus.SUCCESS;
-        } catch (IOException e) {
-            log.error("File upload failed: {}, username: {}\n Exception message: {}", file.getName(), currentUser.getUsername(), e.getMessage());
-            return MediaUploadStatus.FAILED;
-        }
-    }
-
-    @Override
-    public String generateTemporaryLinkForReadingFile(final String key) {
-        try {
-            final URL signedUrl = storage.signUrl(BlobInfo.newBuilder(homeworkBucket.getName(), key).build(),
-                    1, TimeUnit.DAYS, Storage.SignUrlOption.signWith(ServiceAccountCredentials.fromStream(new ClassPathResource(credentialsJsonPath).getInputStream())));
-            return signedUrl.toString();
-        } catch (final IOException e) {
-            log.error("Not able to sign url for file: {}", key);
-            throw new SystemException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @Override
-    public byte[] downloadFile(final String key) {
-        return homeworkBucket.get(key).getContent();
-    }
-
-
-    @Override
-    public void deleteFile(final String key) {
-        try {
-            storage.delete(BlobId.of(homeworkBucket.getName(), key));
-        } catch (RuntimeException e) {
-            log.error("File {} was not deleted from external storage", key);
-            throw new SystemException("File deletion failed", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-}
+//package com.cm.common.repository.external.impl;
+//
+//import com.cm.common.exception.SystemException;
+//import com.cm.common.model.enumeration.MediaUploadStatus;
+//import com.cm.common.repository.external.FileRepository;
+//import com.cm.common.security.AppUserDetails;
+//import com.cm.common.util.AuthorizationUtil;
+//import com.google.auth.oauth2.ServiceAccountCredentials;
+//import com.google.cloud.storage.BlobId;
+//import com.google.cloud.storage.BlobInfo;
+//import com.google.cloud.storage.Bucket;
+//import com.google.cloud.storage.Storage;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.net.URL;
+//import java.util.concurrent.TimeUnit;
+//
+//@Slf4j
+//@Repository
+//@RequiredArgsConstructor
+//public class FileRepositoryImpl implements FileRepository {
+//
+////    private final Bucket homeworkBucket;
+//    private final Storage storage;
+//
+//    @Value("${google.cloud.credentials-json-path}")
+//    private String credentialsJsonPath;
+//
+//    @Override
+//    public MediaUploadStatus uploadFile(final MultipartFile file, final String key) {
+//        final AppUserDetails currentUser = (AppUserDetails) AuthorizationUtil.getCurrentUser();
+//        try {
+////            homeworkBucket.create(key, file.getInputStream());
+//            log.info("Homework uploaded for user: {}", currentUser.getUsername());
+//            return MediaUploadStatus.SUCCESS;
+//        } catch (IOException e) {
+//            log.error("File upload failed: {}, username: {}\n Exception message: {}", file.getName(), currentUser.getUsername(), e.getMessage());
+//            return MediaUploadStatus.FAILED;
+//        }
+//    }
+//
+//    @Override
+//    public String generateTemporaryLinkForReadingFile(final String key) {
+//        try {
+////            final URL signedUrl = storage.signUrl(BlobInfo.newBuilder(homeworkBucket.getName(), key).build(),
+//                    1, TimeUnit.DAYS, Storage.SignUrlOption.signWith(ServiceAccountCredentials.fromStream(new ClassPathResource(credentialsJsonPath).getInputStream())));
+//            return signedUrl.toString();
+//        } catch (final IOException e) {
+//            log.error("Not able to sign url for file: {}", key);
+//            throw new SystemException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//
+//    @Override
+//    public byte[] downloadFile(final String key) {
+//        return homeworkBucket.get(key).getContent();
+//    }
+//
+//
+//    @Override
+//    public void deleteFile(final String key) {
+//        try {
+//            storage.delete(BlobId.of(homeworkBucket.getName(), key));
+//        } catch (RuntimeException e) {
+//            log.error("File {} was not deleted from external storage", key);
+//            throw new SystemException("File deletion failed", HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//}
--- src/main/java/com/cm/common/service/media/file/impl/FileUploadServiceImpl.java
+++ src/main/java/com/cm/common/service/media/file/impl/FileUploadServiceImpl.java
@@ -21,7 +21,7 @@ import java.nio.file.StandardCopyOption;
 @RequiredArgsConstructor
 public class FileUploadServiceImpl implements FileUploadService {
 
-    private final FileRepository fileRepository;
+    private final FileRepository fileRepository = null;
     private final Path localStoragePath = Path.of("upload_failed_files");
 
 
